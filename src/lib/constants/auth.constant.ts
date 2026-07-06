489739bca07fbddc9e54eb1414d9aeb16a509ca8
Amr2231 <amr540290@gmail.com>
2026-07-06 18:28:55 +0300

    feat: add app-wide constants: auth.constant.ts, global.constant.ts

---
+++ src/lib/constants/auth.constant.ts
@@ -0,0 +1,10 @@
+export const FORGOT_PASSWORD_STEPS = {
+  EMAIL: "email",
+  DONE: "done",
+} as const;
+
+export const REGISTER_STEPS = {
+  EMAIL: "email",
+  OTP: "otp",
+  DETAILS: "details",
+} as const;
